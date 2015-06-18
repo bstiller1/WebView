f46b5e1554750387b36e5825c7ba4b040eef9408
Blake Stiller <stiller.blake@gmail.com>
2015-06-18 17:08:24 -0400

    Android
    ePub Lite, added menu to the action bar, using "/res/menu/options.xml"
    and "/res/values/strings.xml"

--- app/src/main/java/com/blakestiller/webview/MainActivity.java
+++ app/src/main/java/com/blakestiller/webview/MainActivity.java
@@ -6,20 +6,61 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends Activity {
     WebView browser;
+    public static String getHtml(String address) throws  IOException, Exception {
+        /* HttpClient httpClient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpClient.execute(httpGet, localContext); */
+        URL url = new URL(address);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        String result = "";
+
+           /* String line = null;
+            while ((line = response.readLine()) != null) {
+                result += line + "\n";
+            } */
+        String response = IOUtils.toString(in, "UTF-8");
+        return response;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        browser=(WebView)findViewById(R.id.webView);
-        //browser.loadUrl("http://blakestiller.com/hfpc/");
-        browser.loadDataWithBaseURL("http://m.blakestiller.com/index.html", "text/html", "utf-8", null, "http://m.blakestiller.com/index.html");
-        browser.getSettings().setJavaScriptEnabled(true);
-       browser.setWebChromeClient(new WebChromeClient());
+        browser = (WebView) findViewById(R.id.webView);
+            //String url = "http://blakestiller.com/hfpc/index.html";
+            //browser.setWebViewClient(new WebViewClient());
+            WebSettings settings = browser.getSettings();
+            settings.setJavaScriptEnabled(true);
+            browser.setWebChromeClient(new WebChromeClient());
+            //browser.loadUrl("http://blakestiller.com/hfpc/");
+            browser.loadDataWithBaseURL("http://blakestiller.com/hfpc/assets/", getHtml("http://blakestiller.com/hfpc/index.html"), "text/html", "UTF-8", "http://blakestiller.com/hfpc/");
+
     }
 
 
@@ -44,4 +85,5 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
